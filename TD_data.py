c2c7f6b99f27767060325afa975935d809b277bb
yongbo Yu <yongboyu@yongbos-MacBook-Air.local>
2024-09-01 10:50:38 -0300

    Using beautiful soup to extract the data from column, which works a bit better than selenium

--- TD_data.py
+++ TD_data.py
@@ -1,9 +1,5 @@
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+# Import the required libraries
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,6 +12,16 @@ from collections import defaultdict
 import pandas as pd
 import undetected_chromedriver as uc
 import gc
+from bs4 import BeautifulSoup
+
+# ####These section seems like for Mac ONLY#######
+import ssl
+import certifi
+# Use certifi for SSL certificates
+os.environ['SSL_CERT_FILE'] = certifi.where()
+os.environ['SSL_CERT_DIR'] = os.path.dirname(certifi.where())
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+# ####These section seems like for Mac ONLY#######
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -28,12 +34,12 @@ def accept_cookies():
     """
     try:
         # Wait for the cookies banner to be present in the DOM
-        cookies_banner = WebDriverWait(driver, 10).until(
+        cookies_banner = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.ID, "onetrust-banner-sdk"))
         )
 
         # Click the Accept All Cookies button
-        accept_all_button = WebDriverWait(cookies_banner, 10).until(
+        accept_all_button = WebDriverWait(cookies_banner, 15).until(
             EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
         )
         accept_all_button.click()
@@ -74,7 +80,6 @@ def click_120_days():
         print(f"An error occurred: {str(e)}")
 
 
-
 def login_to_td(url, username, password):
     """This function logs into the TD Online Bank website using the provided credentials.
 
@@ -85,14 +90,15 @@ def login_to_td(url, username, password):
     """
     driver.get(url)
 
-    username_field = WebDriverWait(driver, 10).until(
+    username_field = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.ID, "username"))
     )
+    print("Username field found.")
     username_field.send_keys(username)
 
     password_field = driver.find_element(By.ID, "uapPassword")
     password_field.send_keys(password)
-
+    accept_cookies()
     login_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'btn-block') and contains(@class, 'td-button-secondary') and contains(., 'Login')]"))
     )
@@ -131,58 +137,45 @@ def login_to_td(url, username, password):
         enter_button.click()
 
         print("Verification code sent.")
-
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: verification code  {e}")
 
 
-def dict_to_csv(data, account_name):
-    """ Store the data in a CSV file.
-
-    Args:
-        data (dict): The data to be stored in the CSV file.
-        account_name (str): The name of the account to be used as the file name.
-    """
-    df = pd.DataFrame(data)
-    account_name = utils.replace_special_chars(account_name)
-    df.to_csv(f"data/{account_name}.csv", index=False)
-    print(f"Data saved to {account_name}.csv")
-
-
-
-def get_accounts(): #only gets the bank accounts
+def get_accounts(type='banking'): #only gets the bank accounts
     """
     Get the list of accounts from the TD Online Bank website.
     """
-    # Found the table container
-    container = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located ((By.ID, "pfsTableCA_BANKBanking"))
-    )
-
-    # Then, find all account links within this container
-    account_rows = WebDriverWait(container, 10).until(
-        EC.presence_of_all_elements_located((
-            By.CSS_SELECTOR, 
-            "tduf-balance-summary-account-row-content.ng-star-inserted"
-        ))
-    )
-
     account_list = []
+    if type == 'banking':
+        # Found the table banking container
+        container = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located ((By.ID, "pfsTableCA_BANKBanking"))
+        )
 
-    for row in account_rows:  
-        try:     
-            account_name_element = WebDriverWait(row, 4).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "a.uf-account-name-and-link"))
-            )
-            #account_name = account_name_element.text.strip()
-            #if 'ACCOUNT' in link.text: # Filter out the account links
-            link_text = account_name_element.text
-            print(link_text)
-            account_list.append(account_name_element.text)
-        except Exception as e:
-            print(f"An error occurred finding account: {e}")
-       
-    return account_list
+        # Then, find all account links within this container
+        account_rows = WebDriverWait(container, 10).until(
+            EC.presence_of_all_elements_located((
+                By.CSS_SELECTOR, 
+                "tduf-balance-summary-account-row-content.ng-star-inserted"
+            ))
+        )
+        # process each account
+        for row in account_rows:  
+            try:     
+                account_name_element = WebDriverWait(row, 4).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "a.uf-account-name-and-link"))
+                )
+                #if 'ACCOUNT' in link.text: # Filter out the account links
+                link_text = account_name_element.text
+                print(link_text)
+                account_list.append(account_name_element.text)
+            except Exception as e:
+                print(f"An error occurred finding account: {e}")
+        
+        return account_list
+    else:
+        print("Type not supported yet.")
+        return None
 
 
 def get_trans_data():
@@ -203,7 +196,6 @@ def get_trans_data():
     
     # Get the table rows
     rows = table.find_elements(By.TAG_NAME, "tr")
-
     for i, row in enumerate(rows):
         if i == 0:
             continue
@@ -215,17 +207,34 @@ def get_trans_data():
         # Get the row cells
         cells = row.find_elements(By.TAG_NAME, "td")
 
-        if len(cells) == 5:
-            date = cells[0].text.strip() if cells[0].text else "N/A"
-            res['Date'].append(date)
-            description = cells[1].text.strip() if cells[1].text else "N/A"
+        # Convert each Selenium WebElement in cells to a BeautifulSoup object
+        cells_soup = [BeautifulSoup(cell.get_attribute('outerHTML'), 'html.parser') for cell in cells]
+
+        if len(cells_soup) == 5:
+            ## Extract date
+            date_cell = cells_soup[0]
+            date_text = date_cell.get_text(strip=True)
+            res['Date'].append(date_text)
+
+            # Handle description
+            description_cell = cells_soup[1]
+            description_div = description_cell.find('div', class_='td-forscreenreader')
+            if description_div:
+                description = description_div.get_text(strip=True).split('View more')[-1].strip()
+            else:
+                description = description_cell.get_text(strip=True).split('View more')[-1].strip()
             res['Description'].append(description)
-            debit = cells[2].text.strip() if cells[2].text else "0.00"
-            res['Withdrawals'].append(debit)
-            credit = cells[3].text.strip() if cells[3].text else "0.00"
-            res['Deposits'].append(credit)
-            balance = cells[4].text.strip() if cells[4].text else "N/A"
+
+            # Extract debit, credit, and balance
+            withdrawls = utils.clean_numeric(cells_soup[2].get_text(strip=True))
+            res['Withdrawals'].append(withdrawls)
+
+            deposits = utils.clean_numeric(cells_soup[3].get_text(strip=True))
+            res['Deposits'].append(deposits)
+
+            balance = utils.clean_numeric(cells_soup[4].get_text(strip=True))
             res['Balance'].append(balance)
+            
 
     return res
 
@@ -238,7 +247,7 @@ def process_accounts(account_list):
     """
     for account in account_list:
 
-        print(f"Downloading transactions for account: {account}")
+        print(f"Downloading transactions for account: {str(account)}")
 
         # Click the checking account
         cur_account_link = WebDriverWait(driver, 10).until(
@@ -253,12 +262,13 @@ def process_accounts(account_list):
         # The following code is optional. It selects the 120 days option.
         # We commented it out because I am tring 30 days option, which is the default.
         #click_120_days()
-        #print('Selected the  120 days option.')
 
-
-        trans_data=get_trans_data()
+        trans_data = get_trans_data()
         # Save the data to a CSV file
-        dict_to_csv(trans_data, account)
+        trans_df = pd.DataFrame(trans_data)
+        csv_name = str(account) + '_td.csv'
+        #csv_name = utils.set_csv_name(account, trans_df, 'td')
+        utils.df_to_csv(trans_df, csv_name, type='banking')
         time.sleep(3)
 
     
@@ -268,7 +278,7 @@ def process_accounts(account_list):
         )
         accounts_page_link.click()
         driver.switch_to.default_content()
-        time.sleep(5)
+        utils.human_like_delay()
 
 
 
@@ -279,13 +289,23 @@ def main():
     options = uc.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--start-maximized")
+    # Add arguments to ignore SSL errors
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    
+    # Disable web security for testing (use with caution)
+    options.add_argument('--disable-web-security')
     driver = uc.Chrome(options=options, enable_cdp_events=True)
 
     print("Driver setup complete.")
 
     print("logging in ....")
     login_to_td(td_utl, TD_USERNAME, TD_PASSWORD)
+
+    print("Get Account List...")
     account_list = get_accounts()
+
+    print("Processing accounts...")
     process_accounts(account_list)
 
 
